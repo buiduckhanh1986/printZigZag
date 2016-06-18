6588b190342603d3ad921444cf9d018c02bd8e60
Bui Duc Khanh <buiduckhanh@hos-macbook-pro.local>
2016-06-18 10:39:55 +0700

    Initial Commit

---
+++ printfZigZag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  printfZigZag
+//
+//  Created by Bui Duc Khanh on 6/18/16.
+//  Copyright © 2016 Bui Duc Khanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ printfZigZag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  printfZigZag
+//
+//  Created by Bui Duc Khanh on 6/18/16.
+//  Copyright © 2016 Bui Duc Khanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
